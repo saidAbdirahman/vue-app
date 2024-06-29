69667acd1e91fc58935a7952cfc18ca26efc7134
saidAbdirahman <saidqaaje11@gmail.com>
2024-06-29 19:48:19 +0200

    ep 02

--- app.js
+++ app.js
@@ -7,18 +7,36 @@ const app = Vue.createApp({
             'showbooks':true,
             'title' : 'The final project',
             'author' : 'Said Abdirahman',
-            'age' : '21'
+            'age' : '21',
+            x : 0,
+            y:0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showbooks = !this.showbooks
+        }, 
+        
+        handleEvent(e,data) {
+            console.log(e,e.type);
+            if(dat){
+                console.log(data)
+            }
+        },
+        Event(){
+            console.log('hi');
+            document.writeln('hi')
+        },
+
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
-     
         // changeTitle(title){
         //      title = prompt('enter the title');
         //     this.title = title
         // }
+,
     }
 });
 
